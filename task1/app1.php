a82b587cf6f247bf9c864f3957b56538cea4d8cc
Adaman Cry <45318496+Adamanr@users.noreply.github.com>
2021-12-13 08:18:35 +0500

    Add files via upload

---
+++ task1/app1.php
@@ -0,0 +1,91 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Rubix</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <ul class="menu">
+            <a href="#" class="logo"><image src="Logg.png"  width="150" height="50"></a>
+            <li class="menuB"><a href="#"><p>Главная</p></a></li>
+            <li class="menuB"><a href="#"><p>О нас</a></li>
+
+            <li class="menuB lastEl"><p>+7 (951) 542 47-11</p></li>
+        </ul>
+
+    </header>
+
+    <section class="carousel" aria-label="Gallery">
+        <ol class="carousel__viewport">
+            <li id="carousel__slide1"
+                tabindex="0"
+                class="carousel__slide">
+                <div class="why">
+                    <h1>доверь свою<br>карьеру <span>rubix</span></h1>
+                    <p>Сочетай свой образ жизни с работой над крутыми проектами, развивайся в строю дружного коллектива, чувствуй себя как дома рядом с мощной техникой!</p>
+                    <div class="btn">
+                        <button>ВОЗЬМИТЕ МЕНЯ!</button>
+                    </div>
+                </div>
+            </li>
+            <li id="carousel__slide2"
+                tabindex="0"
+                class="carousel__slide">
+
+            </li>
+            <li id="carousel__slide3"
+                tabindex="0"
+                class="carousel__slide">
+
+            </li>
+            <li id="carousel__slide4"
+                tabindex="0"
+                class="carousel__slide">
+
+            </li>
+        </ol>
+        <aside class="carousel__navigation">
+            <ol class="carousel__navigation-list">
+                <li class="carousel__navigation-item">
+                    <a href="#carousel__slide1"
+                       class="carousel__navigation-button">Go to slide 1</a>
+                </li>
+                <li class="carousel__navigation-item">
+                    <a href="#carousel__slide2"
+                       class="carousel__navigation-button">Go to slide 2</a>
+                </li>
+                <li class="carousel__navigation-item">
+                    <a href="#carousel__slide3"
+                       class="carousel__navigation-button">Go to slide 3</a>
+                </li>
+                <li class="carousel__navigation-item">
+                    <a href="#carousel__slide4"
+                       class="carousel__navigation-button">Go to slide 4</a>
+                </li>
+            </ol>
+        </aside>
+    </section>
+    <h2>Работа у нас - это</h2>
+
+    <div class="grid-container">
+        <div class="grid-item item-1"><p><span>Бесконечные</span><br>десерты</p></div>
+        <div class="grid-item item-2"><p><span>Свободный </span><br>дресс-код</p></div>
+        <div class="grid-item item-3"><p><span>крутая</span><br>библиотека</p></div>
+
+    </div>
+
+    <footer>
+        <ul class="menu">
+            <a href="#" class="logo"><image src="Logg.png"  width="150" height="50"></a>
+            <li class="menuB"><a href="#"><p>Главная</p></a></li>
+            <li class="menuB"><a href="#"><p>О нас</a></li>
+            <li class="menuB lastEl"><p>г. Челябинск,<br> Энтузиастов 2, 311</p></li>
+        </ul>
+    </footer>
+</body>
+</html>
